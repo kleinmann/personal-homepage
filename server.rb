b7155ca787f96aab03ff694907bf6c1771b9bd9a
Uwe Kleinmann <u.kleinmann@googlemail.com>
2010-10-07 01:52:03 +0200

    Added static index page and VCF card. Also: basic styling per blueprint.

--- server.rb
+++ server.rb
@@ -7,6 +7,10 @@ require 'haml'
 configure do
   enable :run
   set :views, File.dirname(__FILE__) + '/views'
+  set :public, File.dirname(__FILE__) + '/public'
+  set :server, %w[thin mongrel webrick]
+  set :bind, 'localhost'
+  
   set :haml, {:format => :html5}
 end
 
