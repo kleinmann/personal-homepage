332d162be9e7d6537770672f12cbd51bf3fa08bb
Uwe Kleinmann <u.kleinmann@googlemail.com>
2010-10-15 13:39:04 +0200

    Add teaser functionality (partly) to blog. Change styling of blog to something more noticeable. Also: layout changes.

--- lib/blog/blog_post.rb
+++ lib/blog/blog_post.rb
@@ -1,10 +1,13 @@
 class BlogPost
-	attr_reader :title, :text, :created_at, :index
+	attr_reader :title, :text, :created_at, :index, :teaser
 	
 	def initialize(text, created_at, index)
 		@title = text.lines.to_a[0]
 		@text = text.lines.to_a[1..-1].join
 		@created_at = Date.strptime(created_at, "%Y%m%d")
 		@index = index
+		
+		#TODO: Create teaser text
+		@teaser = @text
 	end
 end
