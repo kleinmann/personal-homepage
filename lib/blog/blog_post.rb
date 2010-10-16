db972dfdeccde22dedc0fe5621bfe38f61c3eea9
Uwe Kleinmann <u.kleinmann@googlemail.com>
2010-10-16 13:42:45 +0200

    Finished teaser functionality in blog. Break occurs at a line of one or more "^" and nothing else.

--- lib/blog/blog_post.rb
+++ lib/blog/blog_post.rb
@@ -3,11 +3,29 @@ class BlogPost
 	
 	def initialize(text, created_at, index)
 		@title = text.lines.to_a[0]
-		@text = text.lines.to_a[1..-1].join
 		@created_at = Date.strptime(created_at, "%Y%m%d")
 		@index = index
-		
-		#TODO: Create teaser text
-		@teaser = @text
+		@teaser = create_teaser(text.lines.to_a[1..-1].join)
+		@text = create_text(text.lines.to_a[1..-1].join)
+	end
+	
+	private
+	def create_text(text)
+	  if @teaser != text
+	    text_1 = text[0...(text =~ /^\^+$/)]
+	    text_2 = text[((text =~ /^\^+$/) + 1)..(text.length - 1)]
+	    text_2 = text_2.lines.to_a[1..-1].join
+	    return text_1 + text_2
+	  end
+	  
+	  return text
+  end
+	
+	def create_teaser(text, end_string = "\n[...]")
+	  if text =~ /^\^+$/
+	    teaser = text[0...(text =~ /^\^+$/)] + end_string
+	  else
+	    teaser = text
+    end
 	end
 end
